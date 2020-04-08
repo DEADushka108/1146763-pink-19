9f94f70afd47f76cfb9d010ea0476bf5e54894ee
DEADushka108 <cloxx108@gmail.com>
2020-04-08 16:11:16 +0500

    Добавляет стилизацию мобильной версии всех страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,16 @@
 let btnMenu = document.querySelector(".main-nav__toggle");
 let siteMenu = document.querySelector(".main-nav__list");
+let pageHeader = document.querySelector(".page-header");
 
-function toggleMenu (toggle, menu) {
+function toggleMenu (toggle, menu, bg) {
   toggle.addEventListener('click', function() {
     menu.classList.toggle("main-nav__list--open");
     toggle.classList.toggle("main-nav__toggle--close");
+    bg.classList.toggle("page-header--active");
   });
 }
 
 btnMenu.classList.toggle("main-nav__toggle--close");
 siteMenu.classList.toggle("main-nav__list--open");
-toggleMenu (btnMenu, siteMenu);
+pageHeader.classList.toggle("page-header--active");
+toggleMenu (btnMenu, siteMenu, pageHeader);
