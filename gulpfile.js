1c174fc06d1f9ed972663dd03a84e9a8f8312135
DEADushka108 <cloxx108@gmail.com>
2020-05-01 00:46:53 +0500

    Добавляет сжатие jpeg для улучшения производительности

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,10 @@ gulp.task("images", function () {
   return gulp.src ("build/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
-      imagemin.mozjpeg({progressive: true}),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"));
