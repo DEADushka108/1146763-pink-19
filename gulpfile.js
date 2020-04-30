32c96168ec7c76d239c92e3bdbb7fb92a334c87a
DEADushka108 <cloxx108@gmail.com>
2020-04-30 23:00:19 +0500

    Добавляет не оптимизированный css в сборку

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
@@ -74,7 +75,7 @@ gulp.task("script", function() {
       },
         ignoreFiles: ["*.min.js"]
     }))
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("copy", function () {
